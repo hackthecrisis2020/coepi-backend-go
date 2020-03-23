2749e8108a588cc604f092efea04c256bdb49da1
Rodney Witcher II <rodney@wolk.com>
2020-03-23 11:34:58 -0700

    initial framework / ble

---
+++ ios/coepiios/coepiios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  coepiios
+//
+//  Created by Rodney Witcher on 3/23/20.
+//  Copyright © 2020 coepi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/coepiios/coepiios/ble/BLEDiscovery.swift
@@ -0,0 +1,46 @@
+import Foundation
+import CoreBluetooth
+import os.log
+
+class BLEDiscovery: NSObject, CBCentralManagerDelegate {
+    private let onDiscovered: ((CBPeripheral) -> Void)?
+    private var centralManager: CBCentralManager!
+
+    init(onDiscovered: ((CBPeripheral) -> Void)?) {
+        self.onDiscovered = onDiscovered
+        super.init()
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+    }
+
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        switch central.state {
+        case .unknown:
+            print("BLE unknown")
+//            os_log("BLE unknown", log: bluetoothLog)
+        case .resetting:
+            print("BLE resetting")
+//            os_log("BLE resetting", log: bluetoothLog)
+        case .unsupported:
+            print("BLE unsupported")
+//            os_log("BLE unsupported", log: bluetoothLog)
+        case .unauthorized:
+            print("BLE unauthorized")
+//            os_log("BLE unauthorized", log: bluetoothLog)
+        case .poweredOff:
+            print("BLE poweredOff")
+//            os_log("BLE poweredOff", log: bluetoothLog)
+        case .poweredOn:
+            print("BLE poweredOn")
+//            os_log("BLE poweredOn", log: bluetoothLog)
+            centralManager.scanForPeripherals(withServices: nil)
+        @unknown default:
+            print("BLE default")
+//            os_log("BLE default", log: bluetoothLog)
+        }
+    }
+
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral,
+                        advertisementData: [String: Any], rssi RSSI: NSNumber) {
+        onDiscovered?(peripheral)
+    }
+}
